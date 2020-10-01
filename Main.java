bccc0fca7dc02f7cd9b76842a25a88d37a49a7fe
blisn8405 <blisn8405@wrdsb.ca>
2020-10-01 15:51:53 +0000

    finished q2

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+     // create a scanner for user input
+Scanner input = new Scanner(System.in); 
+    // ask the user if the animal has feathers
+ System.out.println("Does the animal have feathers?");
+ String featherAnswer = input.nextLine();
+
+
+ // check to see the feather status
+ if(featherAnswer.equals("yes")){
+//see if the animal can swim
+System.out.println("Can the animal swim? (yes/no)");
+String swimAnswer = input.nextLine();
+if(swimAnswer.equals("yes")){
+
+}
+System.out.println("This is a duck");
+ } else
+// check to see the feather status
+ if(featherAnswer.equals("no")){
+//see if the animal can swim
+System.out.println("Does the animal have legs? (yes/no)");
+String legsAnswer = input.nextLine();
+if(legsAnswer.equals("yes")){
+System.out.println("This is a lizard");
+ }
+ }
   }
 }
